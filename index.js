f26e08ec04b34540e54325e5c9fea2e6fe37b8ae
Facundo <facundososa676@gmail.com>
2022-09-06 16:37:09 -0300

    desafio con comentarios agregados

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 
-let altura = prompt("Ingrese su altura");
+//Pido por prompt la altura en cm
+let altura = prompt("Ingrese su altura en cm");
+//Pido por prompt la edad
 let edad = prompt("Ingrese su edad");
 
+//Evalua los dos resultados y si uno de ellos es false no deja pasar
 if (altura >= 100 && edad >= 10) {
     alert ("Puede pasar");
 } else {
