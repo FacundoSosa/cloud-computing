8dcf81098ed212333ea3e90e98f0720ac0ac9c23
Facundo <facundososa676@gmail.com>
2022-09-06 16:16:58 -0300

    desafio

---
+++ index.js
@@ -0,0 +1,10 @@
+
+let altura = prompt("Ingrese su altura");
+let edad = prompt("Ingrese su edad");
+
+if (altura >= 100 && edad >= 10) {
+    alert ("Puede pasar");
+} else {
+    alert("No puede pasar");  
+}
+
