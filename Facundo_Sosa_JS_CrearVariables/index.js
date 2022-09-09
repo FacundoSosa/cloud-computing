ebeba65b074b446128e3c551f691da48f296f7ea
Facundo <facundososa676@gmail.com>
2022-09-09 12:40:15 -0300

    Facundo_Sosa_JS_CrearVariables

---
+++ Facundo_Sosa_JS_CrearVariables/index.js
@@ -0,0 +1,5 @@
+let edad;
+let altura;
+
+/* Estos son las variables que se desean medir para determinar si la persona puede entrar o no.
+Considero que hay que hacer un sistema que determine que la edad sea mayor a 10 y la altura mayor a 100cm  */
