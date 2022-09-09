52d25edafc4c96702a420c474179d5ac6497a2dd
Facundo <facundososa676@gmail.com>
2022-09-09 15:19:31 -0300

    DesafioDeCodigo_Depuracion

--- Facundo_Sosa_JS_CrearVariables/index.js
+++ Facundo_Sosa_JS_CrearVariables/index.js
@@ -1,5 +1,47 @@
+//DESAFIO 1: CREAR VARIABLES
+
+//(Comento estas variables para que no me pisen el desafio 2)
+
+/* 
 let edad;
-let altura;
+let altura; 
+*/
 
 /* Estos son las variables que se desean medir para determinar si la persona puede entrar o no.
-Considero que hay que hacer un sistema que determine que la edad sea mayor a 10 y la altura mayor a 100cm  */
+Considero que hay que hacer un sistema que determine que la edad sea mayor a 10 y la altura mayor a 100cm */
+
+
+
+
+//DESAFIO 2: SÚBETE A ESE JUEGO
+
+//=========== STRETCH 1 ===============
+//(Comento el códico para que no me pise las variables del stretch 2)
+
+/* 
+let edad = 10;
+let altura = 120;
+
+//El operador && requiere que las dos variables cumplan con la condición para que de true.
+
+if (edad >= 10 && altura >= 100) { 
+    console.log("¡Súbete, chico!");
+} else {
+    console.log("Lo siento, chico. Tal vez el próximo año");
+} 
+ */
+
+
+//=========== STRETCH 2 ==================
+
+let edad = 9;
+let altura = 120;
+
+//El operador || requiere que una sola de las varibales cumpla con la condición para que de true
+if (edad >= 10 || altura >= 100) { 
+    console.log("¡Súbete, chico!");
+} else {
+    console.log("Lo siento, chico. Tal vez el próximo año");
+}
+
+
---
+++ SOSA_LoopDLoop/index.js
@@ -0,0 +1,23 @@
+let km = 0;
+let velocidad = 9;
+
+
+while (km < 10) {
+    for (km = 3; km <= 10; km *= 3) {
+        if (velocidad < 9) {
+            console.log("Aumenta tu velocidad para obtener un caramelo");
+        } else {
+            console.log("Llegaste al km " + km + " toma un caramelo");
+        } 
+    }; 
+
+    km++; 
+};
+   
+
+    
+
+
+
+
+
